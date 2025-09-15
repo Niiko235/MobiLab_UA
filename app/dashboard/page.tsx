360730ec7ff6ae370d40f0e632076dd19a1925fa
Niiko235 <nicolaseplaza@gmail.com>
2025-09-15 00:12:55 -0500

    init home page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+
+
+
+export default function Page(){
+    return(
+       <div className="flex justify-center items-center h-screen">
+        <iframe
+            src="/games/prueba/index.html"
+            width="800"
+            height="600"
+            className="border rounded-lg shadow-lg"
+        />
+    </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,68 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button'
+import { LoginForm } from '@/features/auth/components/login-form'
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <div className="min-h-screen  flex flex-col ">
+        <nav className="bg-gradient-to-r from-[#667eea] to-[#764ba2] h-24 flex items-center">
+          <div className="ml-28 flex w-1/2 text-3xl text-center gap-2">
+            <span className=" size-10 bg-white rounded-full">🎓</span>
+            <h1 className="font-bold text-white">MoviLab UA</h1>
+          </div>
+          <div className="mr-28 flex w-1/2 gap-8 justify-end">
+            <LoginForm />
+            <Button
+              variant="outline"
+              size="lg"
+              className="text-white bg-[#50178d] text-xl"
+            >
+              Registrarse
+            </Button>
+          </div>
+        </nav>
+        <section className="bg-gradient-to-br from-[#667eea] to-[#764ba2] h-150 flex items-center justify-center flex-col text-center gap-8 text-white">
+          {/* <div className="w-4xl flex flex-col items-center  flex-co gap-8 text-white"> */}
+            <h1 className="font-bold text-7xl">
+              Refuerza tu <br /> conocimiento en física
+            </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <p className="text-2xl font-bold">
+              Plataforma educativa interactiva diseñada especialmente para{' '}
+              <br /> estudiantes de la universidad de la Amazonía. Aprende
+              física <br /> jugando e interactuando con simulaciones
+            </p>
+
+            <Button
+              variant="outline"
+              size="lg"
+              className="text-purple-600 font-bold text-xl hover:bg-purple-600 hover:text-white transition-transform duration-200 hover:-translate-y-1"
+            >
+              Comenzar ahora 🚀
+            </Button>
+          {/* </div> */}
+        </section>
+      <footer className="min-h-96 w-full bg-white text-center pb-16">
+        <h1 className='mt-10 font-bold text-4xl'>¿Por qué elegir MoviLab UA?</h1>
+        <div className='flex flex-wrap justify-center items-center gap-8 mt-10'>
+          <div className='w-96 bg-blue-50 rounded-2xl h-64 flex flex-col justify-center gap-6 p-4'>
+            <span className='text-4xl'>🎮</span>
+            <h1 className='font-bold text-2xl'>Aprendizaje Interactivo</h1>
+            <p>Juegos y simulaciones que hacen el aprendizaje divertido y efectivo</p>
+          </div>
+          <div className='w-96 bg-green-50 rounded-2xl h-64 flex flex-col justify-center gap-6 p-4'>
+            <span className='text-4xl'>📚</span>
+            <h1 className='font-bold text-2xl'>Documentación Completa</h1>
+            <p>Teoría y fórmulas organizadas por temas para consulta rápida</p>
+          </div>
+          <div className='w-96 bg-purple-50 rounded-2xl h-64 flex flex-col justify-center gap-6 p-4'>
+            <span className='text-4xl'>🫂</span>
+            <h1 className='font-bold text-2xl'>Sistema de Records</h1>
+            <p>Sigue tu progreso y compite con tus compañeros</p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
       </footer>
-    </div>
-  );
+      </div>
+    </>
+  )
 }
