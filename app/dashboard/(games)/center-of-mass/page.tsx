83cc952389fb9437fa16adb0465bbb968219d223
Niiko235 <nicolaseplaza@gmail.com>
2025-09-22 21:33:02 -0500

    add game center of mass version 2

--- app/dashboard/(games)/center-of-mass/page.tsx
+++ app/dashboard/(games)/center-of-mass/page.tsx
@@ -1,11 +1,6 @@
 'use client'
 
-import { CardItem } from '@/features/games/center-of-mass/components/car-item'
-
-
 export default function Page() {
-
-
   return (
     <div className="flex flex-row m-4 gap-2">
       <div className="h-150 w-250">
@@ -13,20 +8,6 @@ export default function Page() {
           src="/games/center-of-mass/index.html"
           className="h-150 w-250 border rounded-lg shadow-lg"
         ></iframe>
-      </div>
-      <div className="flex flex-row gap-2 flex-wrap p-2 ">
-        <CardItem
-          title="Tomate"
-          weight={7}
-          url="/games/center-of-mass/assets/sprites_16x16/tomato.png"
-          name='tomato'
-        />
-        <CardItem
-          title="Peperoni"
-          weight={10}
-          url="/games/center-of-mass/assets/sprites_16x16/carrot.png"
-          name='carrot'
-        />
       </div>
     </div>
   )
