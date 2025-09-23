b10878cc70d59389eeff7ea7a447fe9463c4b010
Niiko235 <nicolaseplaza@gmail.com>
2025-09-23 15:16:16 -0500

    add cards game center-of-mass

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,36 +11,13 @@
 //     )
 // }
 
-import {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardAction,
-  CardDescription,
-  CardContent,
-} from '@/components/ui/card'
+
+import { CardPresentation } from '@/features/dashboard/components/card-presentation'
+
+
 
 export default function Page() {
   return (
-    <div className="h-screen font-bold m-2">
-      <h1>Dashboard en proceso...</h1>
-      <section className='flex flex-col gap-4 md:flex-row'>
-        <Card>
-          <CardContent>
-            <p className="text-xl font-bold text-blue-400">Partidas jugadas</p>
-            <p>127</p>
-            <p>total de partidas</p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent>
-            <p className="text-xl font-bold text-blue-400">Partidas jugadas</p>
-            <p>127</p>
-            <p>total de partidas</p>
-          </CardContent>
-        </Card>
-      </section>
-    </div>
+   <CardPresentation></CardPresentation>
   )
 }
